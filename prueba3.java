5112da219bfe2283d515a6a294adefbd4bf041d9
chutia <itssergiocz@gmail.com>
2024-03-12 13:07:03 +0100

    Cuarto commit

---
+++ prueba3.java
@@ -0,0 +1,1 @@
+ // autor sergio Cerrada
---
+++ prueba4.java
@@ -0,0 +1,9 @@
+public class prueba4{
+	private  numero int ;
+ 	public prueba () {
+	 
+ }
+ 	public void ponerNumero(int numeroPoner) {
+	 numero = numeroPoner;
+ }
+}
